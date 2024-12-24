84253c860e109b5b22e9d9257ddbf714b92dcda9
HAMSI0904 <hamsawathana4321@gmail.com>
2024-12-24 18:37:33 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,2 +1,13 @@
 # Hello-python
 print("Hello, world")
+import requests
+
+url = "https://raw.githubusercontent.com/HAMSI0904/Hello-python/main/Hello.py"
+response = requests.get(url)
+
+if response.status_code == 200:
+    script_content = response.text
+    print("Script fetched successfully:\n")
+    print(script_content)
+else:
+    print("Failed to fetch the script. Status code:", response.status_code)
